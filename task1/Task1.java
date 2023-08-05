04e29567311bf7df81d7245ba119c9be319202d6
psevdomatematik <104690117+psevdomatematik@users.noreply.github.com>
2023-08-05 19:21:47 +0700

    Add files via upload

--- task1/Task1.java
+++ task1/Task1.java
@@ -1,6 +1,3 @@
-package Package1;
-//Main-Class: Package3.Task3
-//Class-Path: lib/json-simple.jar
 public class Task1 {
     public static void main(String [] args) {
         int n = Integer.parseInt(args[0]);
